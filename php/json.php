9503910533171450b864470208ec1f9f5e625293
Vagelis Papas <papas@source.gr>
2021-11-29 17:39:34 +0200

    added `dd` and `getInput`

--- php/json.php
+++ php/json.php
@@ -1,6 +1,7 @@
 <?php
     function sendReply($a)
     {
+        header('Content-Type: application/json');
         die(json_encode([
             'error' => 0,
             'data' => $a,
@@ -10,9 +11,20 @@
 
     function sendError($code = 1, $message = 'error')
     {
+        header('Content-Type: application/json');
         die(json_encode([
             'error' => $code,
             'message' => $message,
             'data' => null,
         ]));
     }
+
+    function dd($a) {
+        var_dump($a);
+        die();
+    }
+
+    function getInput() {
+        return json_decode(file_get_contents('php://input'));
+    }
+
